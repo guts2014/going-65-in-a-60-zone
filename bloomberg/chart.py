ed95bae2438cd75db2310276aef0a4307befa4bf
Adam <geek2008@virginmedia.com>
2014-10-11 22:04:33 +0100

    test

--- bloomberg/chart.py
+++ bloomberg/chart.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext
 from API import *
 import json
 
@@ -54,3 +55,18 @@ def zoom_circles_page(request):
 def zoom_circles_data(request):
     print("Starting to get data")
     return HttpResponse(json.dumps(generate_json()))
+    
+def history(request):
+    # Request the context of the request.
+    # The context contains information such as the client's machine details, for example.
+    context = RequestContext(request)
+
+    
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'chart/historyChart.html', context_dict)
--- bloomberg/urls.py
+++ bloomberg/urls.py
@@ -5,6 +5,8 @@ urlpatterns = patterns('',
     url(r'^status/', 'bloomberg.views.connection_status'),
     url(r'^sector/(?P<sector_type>\w+)/$', 'bloomberg.views.sector'),
     
+    url(r'^chart/history', 'bloomberg.chart.history'),
+    
     url(r'^chart/zoomcircles', 'bloomberg.chart.zoom_circles_page'),
     url(r'^data/zoomcircles', 'bloomberg.chart.zoom_circles_data'),
     # url(r'^main/(?P<stock_num>\w+)/', 'bloomberg.views.main'),
