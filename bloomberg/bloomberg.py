f1222050a635b70299e0d1545ab30f8c7da7c2dd
Donald AC Martin <donaldacmartin@gmail.com>
2014-10-11 07:59:13 +0100

    Adding Bloomberg abstraction
    Hide the nasty stuff

---
+++ bloomberg/bloomberg.py
@@ -0,0 +1,23 @@
+# Abstraction layer over Bloomberg API
+
+def get_new_session():
+    options = parseCmdLine()
+    
+    session_options = blpapi.SessionOptions()
+    session_options.setServerHost("10.8.8.1")
+    session_options.setServerPort(8194)
+    
+    print("Connecting to Bloomberg server")
+    session = blpapi.Session(session_options)
+    
+    if not session.start():
+        return None
+    
+    return session
+
+def session_subscribe(session, requests):
+    session.openService("//blp/mktdata")
+    subscriptions = blpapi.SubscriptionList()
+    subscriptions.add(requests)
+    session.subscribe(subscriptions)
+    return session
