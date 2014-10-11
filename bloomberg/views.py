d8bcbef5d9d0286ee75fde5464b7fb4647c9b5a6
Adam <geek2008@virginmedia.com>
2014-10-11 09:59:48 +0100

    Changes some tabs to spaces

--- bloomberg/views.py
+++ bloomberg/views.py
@@ -11,24 +11,6 @@ def connection_status(request):
 
 
 def main(request):
-<<<<<<< HEAD
-	# Request the context of the request.
-	# The context contains information such as the client's machine details, for example.
-	context = RequestContext(request)
-	
-	stock_num = request.GET.get('stock', '')
-	
-	# Construct a dictionary to pass to the template engine as its context.
-	# Note the key boldmessage is the same as {{ boldmessage }} in the template!
-	context_dict = {'lol':stock_num}
-	
-	print(stock_num)
-	
-	# Return a rendered response to send to the client.
-	# We make use of the shortcut function to make our lives easier.
-	# Note that the first parameter is the template we wish to use.
-	return render(request, 'MainPage.html', context_dict)
-=======
     # Request the context of the request.
     # The context contains information such as the client's machine details, for example.
     context = RequestContext(request)
@@ -45,4 +27,3 @@ def main(request):
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render(request, 'MainPage.html', context_dict)
->>>>>>> 774c9908e1f58930dafc72015ca9c21dedcd4294
